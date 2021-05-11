d652fbd349194fa8b6468bfeca958742ae3c4121
JEdwardsii <61206403+JEdwardsii@users.noreply.github.com>
2021-05-11 11:02:09 +0200

    Update GitHub-tutorial-20210511.R

--- GitHub-tutorial-20210511.R
+++ GitHub-tutorial-20210511.R
@@ -6,3 +6,6 @@
 
 # Step 2. We have committed the first change 
 # Now I am adding some more text so we can commit another change 
+# After saving the file, the changes will appear in a tab in the desktop app. You can then commit these changes. After committing, you can revert or discard changes. This should revert changes in the file itself as well. 
+
+# If file is too big to commit and an error appears, *DO NOT CLICK COMMIT ANYWAY*
