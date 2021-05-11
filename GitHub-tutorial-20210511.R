433b5498991612d0b16ebd5f5833cc3d71d2f7cd
JEdwardsii <61206403+JEdwardsii@users.noreply.github.com>
2021-05-11 10:56:45 +0200

    Update GitHub-tutorial-20210511.R

--- GitHub-tutorial-20210511.R
+++ GitHub-tutorial-20210511.R
@@ -2,4 +2,7 @@
 #           GitHub Course - Day 1    
 #==================================================
 
+# Step 1. Save the file into the local repo (save R script into the GitHub-tutorial folder on my computer)
 
+# Step 2. We have committed the first change 
+# Now I am adding some more text so we can commit another change 
